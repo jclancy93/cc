e4cd06c5110d762087e5860ad0e81a508ee3735f
Jack Clancy <jack.clancy93@gmail.com>
2016-01-03 16:11:15 -0500

    first commit

---
+++ CC/FeedCell.swift
@@ -0,0 +1,24 @@
+//
+//  FeedCell.swift
+//  CC
+//
+//  Created by Luka Ivicevic on 12/23/15.
+//  Copyright © 2015 jaysl. All rights reserved.
+//
+
+import UIKit
+
+class FeedCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
